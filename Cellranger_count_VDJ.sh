f567a46e4e9b2fa5547ed15f6cbec1f728e02e5b
Bei-lab <52922045+bei-lab@users.noreply.github.com>
2020-05-13 20:01:31 +0800

    Update Cellranger_count_VDJ.sh

--- Cellranger_count_VDJ.sh
+++ Cellranger_count_VDJ.sh
@@ -1,4 +1,7 @@
 ##----------------------- Cellranger vdj --------------####
+# /data/home/heshuai/software/cellranger-3.1.0/cellranger-cs/3.1.0/bin
+# cellranger --version (3.1.0)
+# Copyright (c) 2019 10x Genomics, Inc.  All rights reserved.
 
 #BSUB -n 16
 #BSUB -o %J.our
@@ -18,6 +21,10 @@ cellranger vdj \
 
 
 ##-------------------- Cellranger count----------------###
+# /data/home/heshuai/software/cellranger-3.0.1/cellranger-cs/3.0.1/bin
+# cellranger --version (3.0.1)
+# Copyright (c) 2019 10x Genomics, Inc.  All rights reserved.
+
 #BSUB -n 16
 #BSUB -o %J.our
 #BSUB -e %J.err
@@ -33,3 +40,4 @@ cellranger count \
 /data4/heshuai/RAW_data/1-SingleCell/2-NKTCL-SingcleCell/NKTCL_SC1811_YGY_lung/AHY725CCXY/AHY725CCXY/sample_name_3,\
 /data4/heshuai/RAW_data/1-SingleCell/2-NKTCL-SingcleCell/NKTCL_SC1811_YGY_lung/AHY725CCXY/AHY725CCXY/sample_name_4 \
 --sample=sample_name
+

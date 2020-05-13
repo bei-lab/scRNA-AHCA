57d812440942be5eeee3b4d9c74b5af5bc61bf7b
Bei-lab <52922045+bei-lab@users.noreply.github.com>
2020-05-13 19:41:24 +0800

    Create CellPhoneDB.sh

---
+++ CellPhoneDB.sh
@@ -0,0 +1,10 @@
+cellphonedb method statistical_analysis \
+       --project-name tissue \
+       --iterations 1000 \
+       --threshold 0.2 \
+       --result-precision 3 \
+       --output-path /data4/heshuai/RAW_data/1-SingleCell/3-HCA/3-analysis/6-Cells_Communications \
+       --verbose \
+       --threads 16 \
+       /data4/heshuai/RAW_data/1-SingleCell/3-HCA/3-analysis/6-Cells_Communications/tissue_cDNA_Cell_type_modified.txt \
+       /data4/heshuai/RAW_data/1-SingleCell/3-HCA/3-analysis/6-Cells_Communications/tissue_cDNA_normalized_expression.txt 

d9214152be3fa67ef33602544a4c92f9553a52d4
Bei-lab <52922045+bei-lab@users.noreply.github.com>
2020-05-13 19:48:10 +0800

    Create Bcl2fastq.sh

---
+++ Bcl2fastq.sh
@@ -0,0 +1,15 @@
+bcl2fastq  \
+--minimum-trimmed-read-length 8 \
+--mask-short-adapter-reads 8  \
+--create-fastq-for-index-reads  \
+--ignore-missing-positions  \
+--ignore-missing-filter  \
+--ignore-missing-bcls  \
+--use-bases-mask=Y150n,I8,n*,Y150n  \
+-R /data4/heshuai/RAW_data/1-SingleCell/3-HCA/0-BCLdata/190109_ST-E00203_0750_BHWF27CCXY_RSJCX1901090001 \
+--output-dir=/data4/heshuai/RAW_data/1-SingleCell/3-HCA/1-Fastq/BHWF27CCXY_lung_pancreas  \
+--interop-dir=/data4/heshuai/RAW_data/1-SingleCell/3-HCA/1-Fastq/BHWF27CCXY_lung_pancreas/i_path  \
+--sample-sheet=/data4/heshuai/RAW_data/1-SingleCell/3-HCA/1-Fastq/cellranger-samplesheet3.csv \
+-r 6 \
+-w 6 \
+--tiles s_[3-4]
